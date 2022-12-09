1abffb7172713b9eeba709f589e5dd3330ad3081
Fksouss <fahdamineksouss@gmail.com>
2022-12-09 01:26:43 +0100

    update the checks

--- src/check_need_of_act.c
+++ src/check_need_of_act.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   checks.c                                           :+:      :+:    :+:   */
+/*   check_need_of_act.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: faksouss <faksouss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 06:01:42 by deman_wolf        #+#    #+#             */
-/*   Updated: 2022/12/09 01:18:42 by faksouss         ###   ########.fr       */
+/*   Updated: 2022/12/09 01:26:20 by faksouss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,4 +24,9 @@ int	check_to_push(t_list *st)
 		tmp = tmp->next;
 	}
 	return (0);
+}
+
+int	check_sa_need(t_list *st, void (*f)(t_list **, t_list *))
+{
+	find_best_mark(&st, (f));
 }
--- src/send_a_to_b.c
+++ src/send_a_to_b.c
@@ -6,13 +6,13 @@
 /*   By: faksouss <faksouss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 23:50:49 by faksouss          #+#    #+#             */
-/*   Updated: 2022/12/09 01:13:28 by faksouss         ###   ########.fr       */
+/*   Updated: 2022/12/09 01:24:39 by faksouss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"../inc/push_swap.h"
 
-void	send_a_to_b(t_list **sta, t_list **stb)
+void	send_a_to_b(t_list **sta, t_list **stb, void (*f)(t_list **, t_list *))
 {
 	while (check_to_push(*sta))
 	{
